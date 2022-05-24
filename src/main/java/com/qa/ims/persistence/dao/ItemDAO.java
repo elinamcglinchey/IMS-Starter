babbe712539d256bf693a3e5b4351acb42842974
unknown <elinamcglinchey1@gmail.com>
2022-05-24 15:43:58 +0100

    adding more changes

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,149 @@
+package com.qa.ims.persistence.dao;
+
+	import java.sql.Connection;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.sql.Statement;
+	import java.util.ArrayList;
+	import java.util.List;
+	 
+	import org.apache.logging.log4j.LogManager;
+	import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+	 
+	public class ItemDAO implements Dao<Item> {
+		
+
+		public static final Logger LOGGER = LogManager.getLogger();
+
+		@Override 
+		public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+			Long id = resultSet.getLong("id");
+			String firstName = resultSet.getString("first_name");
+			String surname = resultSet.getString("surname");
+			String username = resultSet.getString("username");
+			String password = resultSet.getString("password");
+			return new Item(id, firstName, surname, username, password);
+		}
+	 
+		/**
+		 * Reads all customers from the database
+		 * 
+		 * @return A list of customers
+		 */
+		@Override
+		public List<Item> readAll() {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+				List<Item> items = new ArrayList<>();
+				while (resultSet.next()) {
+					items.add(modelFromResultSet(resultSet));
+				}
+				return item;
+			} catch (SQLException e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return new ArrayList<>();
+		}
+
+		public Item readLatest() {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Creates a customer in the database
+		 * 
+		 * @param customer - takes in a customer object. id will be ignored
+		 */
+		@Override
+		public Item create(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("INSERT INTO items(first_name, surname) VALUES (?, ?)");) {
+				statement.setString(1, item.getFirstName());
+				statement.setString(2, item.getSurname());
+				statement.executeUpdate();
+				return readLatest();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		@Override
+		public Item read(Long id) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
+				statement.setLong(1, id);
+				try (ResultSet resultSet = statement.executeQuery();) {
+					resultSet.next();
+					return modelFromResultSet(resultSet);
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Updates a customer in the database
+		 * 
+		 * @param customer - takes in a customer object, the id field will be used to
+		 *                 update that customer in the database
+		 * @return
+		 */
+		@Override
+		public Item update(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("UPDATE items SET first_name = ?, surname = ? WHERE id = ?");) {
+				statement.setString(1, item.getFirstName());
+				statement.setString(2, item.getSurname());
+				statement.setLong(3, item.getId());
+				statement.executeUpdate();
+				return read(item.getId());
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Deletes a customer in the database
+		 * 
+		 * @param id - id of the customer
+		 */
+		@Override
+		public int delete(long id) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("DELETE FROM customers WHERE id = ?");) {
+				statement.setLong(1, id);
+				return statement.executeUpdate();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return 0;
+		}
+
+	}
+
+
